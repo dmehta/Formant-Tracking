3854914cca6d5e14d1ac225b86fc207f73d447d4
dmehta <daryush.mehta@alum.mit.edu>
2010-04-23 14:42:43 -0400

    AR coefficients to cepstral coefficients is now a separate function outside of lpc2c.m
    This allows other functions to call it.

--- filter/lpc2c.m
+++ filter/lpc2c.m
@@ -20,30 +20,5 @@ C = zeros(cepOrder, N);   % Initialize LPCC matrix
 
 % Compute cepstrum for every frame
 for i = 1:N
-    C(:,i) = lp2cf(arCoeffs(:,i),cepOrder);
+    C(:,i) = ar2cp(arCoeffs(:,i),cepOrder);
 end
-
-% Auxililary function implementing the necessary recursion
-function [cepCoeffs] = lp2cf(arCoeffs, cepOrder)
-
-p = length(arCoeffs);  % Number AR coefficients given
-C = zeros(cepOrder,1); % Number Cepstral coefficients to produce
-C(1) = arCoeffs(1);    % Initial case in recursion
-
-% Case 1: index lower than max num AR coeffs
-for j = 2:p
-    a_seq = arCoeffs(j:-1:1);
-    aaInds = 1:1:j-1;
-    c_seq = C(aaInds);
-    C(j) = a_seq(1) + sum(aaInds'.*c_seq(aaInds).*a_seq(aaInds+1))/j;
-end
-
-% Case 2: index higher than max num AR coeffs
-for j = p+1:cepOrder
-    a_seq = arCoeffs(p:-1:1)';
-    c_seq = C(j-p:1:j-1);
-    aaInds = 1:1:length(c_seq);
-    C(j) = sum(aaInds.*c_seq(aaInds)'.*a_seq(aaInds))/p;
-end
-
-cepCoeffs = C; % Set output
